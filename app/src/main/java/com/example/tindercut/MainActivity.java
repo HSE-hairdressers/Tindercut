795d769187f4077aaff443fa05cd8393bb7fd975
NewRonin <44542591+NewRonin@users.noreply.github.com>
2023-03-01 17:49:33 +0300

    scrolling activity addition + separate buttons handle

--- app/src/main/java/com/example/tindercut/MainActivity.java
+++ app/src/main/java/com/example/tindercut/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.tindercut;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +17,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements PickiTCallbacks {
 
     private Bitmap ImageBitmap;
     private Button button;
+    private Button button2;
     private ImageView ImageView;
     private final int picked_image = 1;
     PickiT pickIt;
@@ -67,7 +74,18 @@ public class MainActivity extends AppCompatActivity implements PickiTCallbacks {
                 Intent pickPhoto = new Intent(Intent.ACTION_PICK);
                 pickPhoto.setType("image/*");
                 pickPhoto.setAction(Intent.ACTION_GET_CONTENT);
+                checkPermission();
                 startActivityForResult(pickPhoto, picked_image);
+            }
+        });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ImageBitmap != null) {
+                    sendImage();
+                }
             }
         });
 
@@ -77,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements PickiTCallbacks {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode){
+        switch (requestCode) {
             case picked_image:
                 //Получаем URI картинки:
                 Uri imageUri = data.getData();
@@ -94,76 +112,77 @@ public class MainActivity extends AppCompatActivity implements PickiTCallbacks {
                 ByteArrayOutputStream byteArrayOutputStream;
                 byteArrayOutputStream = new ByteArrayOutputStream();
 
-                if(ImageBitmap != null){
+                if (ImageBitmap != null) {
                     ImageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                    byte [] bytes = byteArrayOutputStream.toByteArray();
-                    final String base64Image  = Base64.encodeToString(bytes, Base64.DEFAULT);
-                    RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                    String url ="http://79.137.206.63:8011/img";
-
-                    SimpleMultiPartRequest uploadRequest = new SimpleMultiPartRequest(Request.Method.POST, url,
-                            new Response.Listener<String>() {
-                                @Override
-                                public void onResponse(String response) {
-                                    try {
-                                        JSONObject responseJSON = new JSONObject(response);
-                                        JSONObject hairdresser = responseJSON.getJSONObject("hairdresser");
-                                        JSONArray hairImages = responseJSON.getJSONArray("images");
-
-                                        //Выполняем проверку успеха отправки изображения
-                                        String requestResult = responseJSON.getString("result");
-                                        String haircutStyle = responseJSON.getString("hairstyle");
-                                        if (requestResult.equals("Ok")){
-                                            Toast.makeText(getApplicationContext(), "Изображение успешно загружено.", Toast.LENGTH_SHORT).show();
-
-                                            //Получсаем изображения
-                                            for(int i = 0; i < hairImages.length(); i++){
-                                                JSONObject hairImage = hairImages.getJSONObject(i);
-                                                String hairImageName = hairImage.getString("name");
-                                                JSONArray hairImageBytes = hairImage.getJSONArray("binary");
-                                                byte[] byteArrayImage = new byte[hairImageBytes.length()];
-                                                for (int j = 0; j < hairImageBytes.length(); j++) {
-                                                    byteArrayImage[j]=(byte)(((int)hairImageBytes.get(j)) & 0xFF);
-                                                }
-                                                //byte[] byteArrayInput = hairImage.getJSONArray("binary");
-                                                System.out.println(hairImage.toString());
-                                                System.out.println(Arrays.toString(byteArrayImage));
-                                                ImageBitmap = BitmapFactory.decodeByteArray(byteArrayImage, 0, byteArrayImage.length);
-                                                //Toast.makeText(getApplicationContext(), hairImage.toString(), Toast.LENGTH_LONG).show();
-                                                ImageView.setImageBitmap(ImageBitmap);
-                                                Toast.makeText(getApplicationContext(), haircutStyle, Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                        else{
-                                            Toast.makeText(getApplicationContext(), requestResult, Toast.LENGTH_SHORT).show();
-                                        }
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-
-                                    }
-                                }
-                            }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                        }});
-                    String fileName = Calendar.getInstance().getTime().toString()+" Photo";
-                    //System.out.println("Путь: "+ imageUri.getPath());
-                    File test = new File(imagePath);
-                    uploadRequest.addFile("image", imagePath);
-                    //uploadRequest.addMultipartParam("body", "text/plain", base64Image);
-
-                    queue.add(uploadRequest);
+                    byte[] bytes = byteArrayOutputStream.toByteArray();
+                    final String base64Image = Base64.encodeToString(bytes, Base64.DEFAULT);
                 }
 
-                // * ТУТ НУЖНО ЧТО-ТО СДЕЛАТЬ НА СЕРВЕРЕ *
 
                 //Отображаем картинку по URI
                 ImageView.setImageBitmap(ImageBitmap);
 
         }
 
+    }
+
+    public void sendImage() {
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://79.137.206.63:8011/img";
+
+        SimpleMultiPartRequest uploadRequest = new SimpleMultiPartRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject responseJSON = new JSONObject(response);
+                            JSONObject hairdresser = responseJSON.getJSONObject("hairdresser");
+                            JSONArray hairImages = responseJSON.getJSONArray("images");
+
+                            //Выполняем проверку успеха отправки изображения
+                            String requestResult = responseJSON.getString("result");
+                            String haircutStyle = responseJSON.getString("hairstyle");
+                            if (requestResult.equals("Ok")) {
+                                Toast.makeText(getApplicationContext(), "Изображение успешно загружено.", Toast.LENGTH_SHORT).show();
+
+                                //Получсаем изображения
+                                for (int i = 0; i < hairImages.length(); i++) {
+                                    JSONObject hairImage = hairImages.getJSONObject(i);
+                                    String hairImageName = hairImage.getString("name");
+                                    JSONArray hairImageBytes = hairImage.getJSONArray("binary");
+                                    byte[] byteArrayImage = new byte[hairImageBytes.length()];
+                                    for (int j = 0; j < hairImageBytes.length(); j++) {
+                                        byteArrayImage[j] = (byte) (((int) hairImageBytes.get(j)) & 0xFF);
+                                    }
+                                    //byte[] byteArrayInput = hairImage.getJSONArray("binary");
+                                    System.out.println(hairImage.toString());
+                                    System.out.println(Arrays.toString(byteArrayImage));
+                                    ImageBitmap = BitmapFactory.decodeByteArray(byteArrayImage, 0, byteArrayImage.length);
+                                    //Toast.makeText(getApplicationContext(), hairImage.toString(), Toast.LENGTH_LONG).show();
+                                    ImageView.setImageBitmap(ImageBitmap);
+                                    Toast.makeText(getApplicationContext(), haircutStyle, Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), requestResult, Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        String fileName = Calendar.getInstance().getTime().toString() + " Photo";
+        //System.out.println("Путь: "+ imageUri.getPath());
+        uploadRequest.addFile(fileName, imagePath);
+        //uploadRequest.addMultipartParam("body", "text/plain", base64Image);
+
+        queue.add(uploadRequest);
     }
 
     @Override
@@ -191,5 +210,28 @@ public class MainActivity extends AppCompatActivity implements PickiTCallbacks {
     @Override
     public void PickiTonMultipleCompleteListener(ArrayList<String> paths, boolean wasSuccessful, String Reason) {
 
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                try {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setData(Uri.parse(String.format("package:%s", getApplicationContext().getPackageName())));
+                } catch (Exception e) {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+
+                }
+            }
+        }
     }
 }
